3bbf9d8363192cff60d72961ef939f09ca33dd7a
Yogurt7v <egorov-2k@yandex.ru>
2025-07-09 21:36:48 +0300

    express to http. просто попытка посмотреть

--- src/server.jsx
+++ src/server.jsx
@@ -1,23 +1,30 @@
 import express from 'express';
 import { readFile } from 'fs/promises';
-import { renderToString } from 'react-dom/server'; // функция превращающая реакт компонент в строку
-// import { renderToStaticMarkup } from 'react-dom/server';
+// import { renderToString } from 'react-dom/server'; // функция превращающая реакт компонент в строку
+import { renderToStaticMarkup } from 'react-dom/server';
 import App from './App';
+import http from 'http';
 
-const app = express();
-const port = 3001;
+// const app = express();
 
-app.get('/', async (req, res) => {
-  const template = await readFile('./index.html', 'utf-8');
-  const html = renderToString(<App />);
-  // const html = renderToStaticMarkup(<App />);
-  res.send(template.replace("<div id='root'></div>", `<div id="root">${html}</div>`));
+http
+  .createServer('/', async (req, res) => {
+    const template = await readFile('./index.html', 'utf-8');
+    // const html = renderToString(<App />);
+    const html = renderToStaticMarkup(<App />);
 
-  // находим в индексе разметку и заменяем её на реакт компонент превращённый в строку
-});
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+
+    res.end(template.replace("<div id='root'></div>", `<div id="root">${html}</div>`));
+
+    // находим в индексе разметку и заменяем её на реакт компонент превращённый в строку
+  })
+  .listen(3001);
 
 app.use(express.static('./dist')); // раздача статики только для примера. лучше используй nginx
 
 app.listen(port, () => {
-  console.log(`Сервер стартовал на порту ${port}`);
+  console.log(`Сервер стартовал на порту 3001`);
 });
