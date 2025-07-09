e5afc7d41d04d5396f5b6821ca4e2ce65feb1598
Yogurt7v <egorov-2k@yandex.ru>
2025-07-09 21:32:06 +0300

    try renderToStaticMarkup

--- src/server.jsx
+++ src/server.jsx
@@ -1,6 +1,7 @@
 import express from 'express';
 import { readFile } from 'fs/promises';
 import { renderToString } from 'react-dom/server'; // функция превращающая реакт компонент в строку
+// import { renderToStaticMarkup } from 'react-dom/server';
 import App from './App';
 
 const app = express();
@@ -9,6 +10,7 @@ const port = 3001;
 app.get('/', async (req, res) => {
   const template = await readFile('./index.html', 'utf-8');
   const html = renderToString(<App />);
+  // const html = renderToStaticMarkup(<App />);
   res.send(template.replace("<div id='root'></div>", `<div id="root">${html}</div>`));
 
   // находим в индексе разметку и заменяем её на реакт компонент превращённый в строку
